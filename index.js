4192b0bef6824d292b9fea716e1e056fa0d260be
Hardi <98042516+MrHardix@users.noreply.github.com>
2022-02-09 13:39:11 -0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('ODMwMjY1NzQyNzk1ODY2MTYy.YHELOg.98E4OmVsYo9kQergTfYqZGFnfEU');
+client.login('OTE3ODE5NjQxNTU0OTUyMjYy.Ya-QJg.9hDDGTtU_XiyeWrX57u_GZqJb2Y');
 client.commands = new Collection();
 client.setMaxListeners(0);    
 client.prefix = PREFIX;
